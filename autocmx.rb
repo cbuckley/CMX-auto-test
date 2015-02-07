6482279dfdb46fa5363d588304a2edb555d70541
root <root@lab-cmx.supportlab.local>
2015-02-07 22:46:43 +0000

    Added storage for clients

--- autocmx.rb
+++ autocmx.rb
@@ -6,6 +6,7 @@ require "sinatra/redirect_with_flash"
 require "json"
 require "yaml"
 require "rack-google-analytics"
+require "data_mapper"
 require "webrick"
 enable :sessions
 
@@ -24,6 +25,9 @@ use Rack::GoogleAnalytics, :tracker => CONFIG['tracker']
 
 puts "Setting up server at #{HOSTNAME}:#{PORT} with the SECRET #{SECRET}"
 
+
+db = "sqlite://#{Dir.pwd}/cmxtests.db"
+
 if (PORT)
 	set :port, PORT
 end
@@ -36,7 +40,7 @@ else
 	DB = Sequel.connect("sqlite://#{Dir.pwd}/cmxtests.db")
 end
 
-DB.create_table? :tests do
+DB.create_table? :tests do #Create the tests table if not exists
 	primary_key :id
 	String :name, :text => true, :null => false
 	String :case, :null => false
@@ -50,9 +54,32 @@ DB.create_table? :tests do
 	DateTime :data_at
 	String :state, :text => true
 end
+DB.create_table? :clients do # Create the clients table if not exists
+	primary_key :id
+	Integer :test
+	String :mac, :key => true 
+	String :seenString
+	Integer :seenMillis, :default =>0
+	Float :lat
+	Float :lng
+	Float :unc
+	String :manufacturer
+	String :os
+	String :ssid
+	String :floors
+	DateTime :updated_at
+	DateTime :created_at
+end
 
 Sequel::Model.plugin :validation_helpers
-
+class Client < Sequel::Model
+	def before_create
+		self.updated_at = Time.now
+	end
+	def before_save
+		self.updated_at = Time.now
+	end
+end
 class Test < Sequel::Model
 	def validate
 		super
@@ -99,55 +126,82 @@ end
 
 #Recieve data
 post "/data/:id" do
-	n = Test.first(:id => params[:id])
-	if n
-		n.data_at = Time.now
-		if request.media_type == "application/json"
+	n = Test.first(:id => params[:id]) #Find the test from the DB
+	if n # if the tests exists
+		currentTest = params[:id]
+		n.data_at = Time.now #Current Time
+		if request.media_type == "application/json" # If response is JSON, parse it
 			request.body.rewind
 			map = JSON.parse(request.body.read)
 		else
 			map = JSON.parse(params['data'])
 		end
-		if map == nil
+		
+		if map == nil # We were not able to parse the post data
 			request.body.rewind
 			logger.warn "#{params[:id]} Could not parse POST body #{request.body.read}"
 			n.state = "bad_post"
 			n.save
 			return
 		end
-		if map['secret'] != n.secret
+
+		if map['secret'] != n.secret #Secret does not match the one in the test
 			logger.warn "#{params[:id]} Got post with bad secret: #{map['secret']}"
 			n.state = "bad_secret"
 			n.save
 			return
 		end
-		logger.info "Version is #{map['version']}"
+	
+		logger.info "Version is #{map['version']}" #API Version
 
 		if map['version'] == '1.0'
 			n.api = 1.0
-			data = map['probing'].to_s
+			data = map['probing'].to_s #V1 stores data in probing var
 		elsif map['version'] == '2.0'
 			n.api = 2.0
-			data = map['data'].to_s
-		else
+			data = map['data'].to_s #V2 stores data in data	
+		else #API Version that we dont know about
 			logger.warn "#{params[:id]} Got post with unknown API version: #{map['version']}"
 			n.api = map['version']
 			n.state = "bad_api"
 			n.save
 			return
 		end
+
 		logger.info "#{params[:id]} Post data are (First 100 characters): #{data[0, 99]}#"
 		n.state = "complete"
 		n.complete = true
-		n.save
-	else
-		logger.info "Received data for test #{params[:id]}, but don't have that configured"
+		n.save #Save the post
+		
+		if n.api == 2.0 #If the API is 2 then we will dump the data to the DB so that the map can use this
+			if map['type'] != 'DevicesSeen'
+				logger.warn "got post for event that we're not interested in: #{map['type']}"
+				return
+			end
+			map['data']['observations'].each do |c|
+				next if c['location'] == nil # We only want to store locations
+				cl = Client.first(:mac => c['clientMac']) || Client.new
+				loc = c['location'];
+				cl.mac = c['clientMac']
+				cl.lat = loc['lat']
+				cl.lng = loc['lng']
+				cl.unc = c['unc']
+				cl.seenString = c['seenTime']
+				cl.seenMillis = c['seenEpoch']
+				cl.floors = map['data']['apFloors'] == nil ? "" : map['data']['apFloors'].join #If this is empty then we store a string otherwise we concatonate them.
+				cl.manufacturer = c['manufacturer']
+				cl.os = cl.os = c['os']
+				cl.test = currentTest
+				cl.save
+			end
+		else
+			logger.info "Received data for test #{params[:id]}, but don't have that configured"
+		end
 	end
 end
-
 # Home Page
 get "/" do 
-	@tests = DB[:tests]
+@tests = DB[:tests]
 	@title = "All Tests"
 	if @tests.empty?
 		flash[:notice] = "No pending tests found. Add your first below."
@@ -242,5 +296,6 @@ get "/:id/complete" do
 		redirect "/", :error => "Error marking test as complete."
 	end
 end
+
 
 
